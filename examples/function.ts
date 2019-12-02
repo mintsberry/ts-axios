49cf7e0a60104365d699dbdb5abeaccf6752428a
mintsberry <mintberfly@gmail.com>
2019-12-02 18:07:57 +0800

    feat: 案例学习

---
+++ examples/function.ts
@@ -0,0 +1,9 @@
+function warn(): void {
+
+}
+
+declare function create(o: object | null) : void 
+
+let sm: any = "this is string"
+// sm.length 
+let smL: number = (sm as string) .length
---
+++ examples/type.ts
@@ -0,0 +1,17 @@
+let decLitergl: number = 20
+let hexLiteral: number = 0x14
+let binLiteral: number = 0b10100
+let ocatalLiteral: number = 0o241
+
+let str: string = 'bob'
+
+let list: number[] =  [1, 2, 3]
+let arr: Array<number> = [1, 2, 3]
+//元组
+let x: [string, number] = ['hello', 10]
+//枚举
+enum Color {
+  Red,
+  Green,
+  Blue
+}
